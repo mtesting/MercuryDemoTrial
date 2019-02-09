19052dc0ab02cb9b434ab63cab8455cda887c734
mtesting <mtesting66@gmail.com>
2019-02-09 22:51:39 +0000

    Initial commit

---
+++ src/main/java/MercuryDemoTrial/App.java
@@ -0,0 +1,13 @@
+package MercuryDemoTrial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
